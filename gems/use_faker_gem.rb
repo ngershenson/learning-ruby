9993be48dc89b04b4de33521043730c3e9d4c4cd
Noah Gershenson <158068575+ngershenson@users.noreply.github.com>
2025-02-16 11:56:33 -0500

    finished course

---
+++ gems/use_faker_gem.rb
@@ -0,0 +1,12 @@
+require "faker"
+
+# puts Faker.class
+# puts Faker::Name.class
+# puts Faker::Name.name
+# puts Faker::Name.name_with_middle
+# puts Faker::Name.first_name
+
+# p Faker.constants.sort
+
+puts Faker::Music.instrument
+puts Faker::Sports::Football.player
